c4b6b221c338d18dc9417af655e967437d17cbbb
angelporlan <angeelporlan4@gmail.com>
2024-11-27 18:47:52 +0100

    add task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from '../components/counter/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CounterComponent],
+  imports: [CommonModule, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

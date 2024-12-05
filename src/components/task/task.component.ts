df00c2935ebac603ffdab2abaebee7c5ee5b01c2
angelporlan <angeelporlan4@gmail.com>
2024-12-05 18:55:48 +0100

    modified the time component and added margins

--- src/components/task/task.component.ts
+++ src/components/task/task.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
+import { TimeComponent } from '../time/time.component';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [ProgressBarComponent],
+  imports: [ProgressBarComponent, TimeComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
-
 }
